6b2b4f9fda91e5ee55f526f610934d2765230a59
ishanrana363 <ishanrana363@gmail.com>
2024-12-09 22:02:46 +0600

    review api reduxt completed

---
+++ src/redux/feature/review/reviewApi.js
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { baseUrl } from './../../../util/baseUrl';
+
+const reviewApi = createApi({
+    reducerPath: "reviewApi",
+    baseQuery: fetchBaseQuery({ baseUrl: baseUrl() }),
+    tagTypes: ["reviews"],
+    endpoints: (builder) => ({
+        postReview: builder.mutation({
+            query: (payload) => ({
+                url: "/post-review",
+                method: "POST",
+                body: payload
+            }),
+            invalidatesTags: (result, error, { postId }) => [{ type: "reviews", id: postId }]
+        }),
+        getReviewCount: builder.query({
+            query: () => ({
+                url: "/total-review",
+                method: "GET"
+            })
+        }),
+
+        getRevieByUserId : builder.query({
+            query: () => ({
+                url: `/product-review}`,
+                method: "GET"
+            }),
+            providesTags: (result)=> result ? [ { type: "reviews", id: result[0]?.email}] : []
+        })
+
+    })
+})
+
+export const {
+    usePostReviewMutation,
+    useGetReviewCountQuery,
+    useGetRevieByUserIdQuery
+} = reviewApi;
+
+export default reviewApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import authApi from './feature/auth/authApi'
 import  authSlice  from './feature/auth/authSlice';
 import productApi from './feature/product/productApi';
+import reviewApi from './feature/review/reviewApi';
 
 export const store = configureStore({
     reducer: {
         [authApi.reducerPath]: authApi.reducer,
         auth : authSlice,
-        [productApi.reducerPath] : productApi.reducer 
+        [productApi.reducerPath] : productApi.reducer,
+        [ reviewApi.reducerPath] : reviewApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authApi.middleware,productApi.middleware),
+        getDefaultMiddleware().concat(authApi.middleware,productApi.middleware,reviewApi.middleware),
 
 });
